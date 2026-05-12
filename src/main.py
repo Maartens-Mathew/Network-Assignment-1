60e4499c01268117d39a7ab84d79f4504a98e23e
Maartens-Mathew <maartens.mathew@gmail.com>
2026-05-12 13:59:51 +0200

    Integrated PySide6 into the project. Busy creating the file and folder structure

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+
+def main() -> None:
+    app = QApplication(sys.argv)
+
+   # Set style of application
+   # app.setStyleSheet(APP_STYLE)
+
+    # define view models that live for lifetime of program
+    # view_model = TaskViewModel()
+    
+    # entry point for first window
+    # window = MainWindow(view_model)
+
+    # window.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
